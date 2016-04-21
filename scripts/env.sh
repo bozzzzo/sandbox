5d0516c6c8927722dc425cac651ae2a7e65f3166
Bozo Dragojevic <bozzo@digiverse.si>
2016-04-21 14:25:30 -0400

    re-encrypt the token

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,1 +1,1 @@
-env | sort | awk -v P="$1" 'BEGIN{FS=OFS="=";if(P!=""){P=P " "}}/TOKEN/{$2="********"}{print P $0}'
+env | sort | awk -v P="$1" 'BEGIN{FS=OFS="=";if(P!=""){P=P " "}}/encrypted|TOKEN/{$2="********"}{print P $0}'
